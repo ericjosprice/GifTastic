7a8c1abb5fd6bec24a1a880d59adb828221c141b
ericjosprice <epriceutexas@gmail.com>
2019-03-14 16:53:08 -0500

    complete

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,50 +1,80 @@
-// Api Key:
-// FVAZwtJAqZB4l8xC0kg6lkJ1hP8m9NFI
+// note to self. items 4 and 6 remain
+// #4 When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
+// #6 Add a form to your page takes the value from a user input box and adds it into your topics array. Then make a function call that takes each topic in the array remakes the buttons on the page.
 
 var aTopics = ["fail", "yeet", "Jeep", "fat", "haters", "memes", ];
 $(document).ready(function () {
 //======================================================================
-
-aTopics.forEach(function (topic) {
-    var btn = $("<button>");
-    btn.text(topic);
-    btn.addClass("btn btn-dark m-1 jif")
-    btn.attr("data-name", topic);
-    $("#buttons").append(btn);
-});
-
-
-////////////////////////////////////////////////////////////////////////
 // functions
 
+// display aTopics on the DOM
+function onLoad(){
+    $("#buttons").empty();
+    console.log(aTopics)
+    aTopics.forEach(function (topic) {
+        var btn = $("<button>");
+        btn.text(topic);
+        btn.addClass("btn btn-dark m-1 topic")
+        btn.attr("data-name", topic);
+        $("#buttons").append(btn);
+    });
+}
+
+// display array of topics as buttons on the DOM
 function displayGifs() {
     var gifs = $(this).attr("data-name");
+    // console.log("displayGifs" + JSON.stringify($(this)));
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifs + "&limit=10"+ "&api_key=FVAZwtJAqZB4l8xC0kg6lkJ1hP8m9NFI";
-
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (res) {
-            console.log(res);
-            console.log(res.data[0].images.original_still.url);
-
             for(var i =0 ; i<res.data.length; i++){
             var newDiv = $("<div>")
             var newImg = $("<img>");
             var newP = $("<p>")
             newP.text("Rating: " + res.data[i].rating)
-            newImg.attr("src", res.data[i].images.original_still.url);
+            newImg.attr("src", res.data[i].images.original_still.url).attr("state","still").attr("class","jif").attr("data-animate", res.data[i].images.fixed_height.url).attr("data-still", res.data[i].images.original_still.url);
             newDiv.append(newImg).append(newP);
             $("#results").prepend(newDiv);
         }
-        });
-
+    });
 }
+// user adds a new topic
+function newTopic(){
+    var sInputValue= $(".form-control").val();
+    if(sInputValue===""){
+        return;
+    }else{
+    aTopics.push(sInputValue);
+    }
+}
+//=============================================================================================
+//main process
+onLoad();
+//click to display gifs on screen
+    $(document).on("click",".topic", displayGifs);
 
-    /////////////////////////////////////////////////////////////////////////
-    // When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page.
+        // When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page.
+    //still or animate toggle click
+    $(document).on("click",".jif", function state(){
+        var state = $(this).attr("state");        
+        if(state==="still"){
+            $(this).attr("state","animate");
+            $(this).attr("src", $(this).attr("data-animate"));
+        } else if(state==="animate"){
+            $(this).attr("state", "still");
+            $(this).attr("src", $(this).attr("data-still"));
+        }
+        
+    });
 
-    $(document).on("click",".jif", displayGifs);
-    var value = $(this).val();
+    //when user clicks submit run the newTopic function
+    $(document).on("click", "#button-addon2", function(){
+    event.preventDefault();
+    newTopic();
+    onLoad();
+});
+  
 
 })//close document.ready
